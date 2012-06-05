50c340b2e6cea4dde5658b2151da1a72a1850a94
Stanislav Chumarin <stanislav.chumarin@gmail.com>
2012-06-05 16:02:31 +0300

    src forgotten

---
+++ TimersFlowApp/src/ua/com/leastas/TimersListActivity.java
@@ -0,0 +1,15 @@
+package ua.com.leastas;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TimersListActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
