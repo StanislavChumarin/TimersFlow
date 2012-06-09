7e07719f3544a219116c93b612e41026623a5906
Stanislav Chumarin <stanislav.chumarin@gmail.com>
2012-06-09 14:13:47 +0300

    Added timer setup layout

---
+++ TimersFlowApp/src/ua/com/leastas/SetupTimerActivity.java
@@ -0,0 +1,30 @@
+package ua.com.leastas;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: stanislavchumarin
+ * Date: 09.06.12
+ * Time: 02:30
+ * This activity manages creation and modification of Timer
+ */
+public class SetupTimerActivity extends Activity {
+
+//    @Override
+//    public void onCreate(Bundle savedInstanceState)
+//    {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.setup_timer_layout);
+//
+//        setTitle(R.string.set_up_timer);
+//    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setup_timer_layout);
+    }
+}
--- TimersFlowApp/src/ua/com/leastas/TimersListActivity.java
+++ TimersFlowApp/src/ua/com/leastas/TimersListActivity.java
@@ -1,56 +1,34 @@
 package ua.com.leastas;
 
 import android.app.Activity;
-import android.app.Dialog;
-import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.TimePicker;
 
-import java.util.Calendar;
+public class TimersListActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
 
-public class TimersListActivity extends Activity
-{
-    /** Called when the activity is first created. */
-
-    private TextView mTimeDisplay;
-    private Button mPickTime;
-
-    private int mHour;
-    private int mMinute;
-
-    static final int TIME_DIALOG_ID = 0;
 
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.timers_list);
 
-        mTimeDisplay = (TextView) findViewById(R.id.timeDisplay);
-
-        // get the current time
-        final Calendar c = Calendar.getInstance();
-        mHour = c.get(Calendar.HOUR_OF_DAY);
-        mMinute = c.get(Calendar.MINUTE);
-
-        // display the current date
-        updateDisplay();
+//        findViewById(R.id.add_row).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(TimersListActivity.this, SetupTimerActivity.class));
+//            }
+//        });
     }
 
     public void addRow(View v) {
-        showDialog(TIME_DIALOG_ID);
+        Intent setupTimerIntent = new Intent(v.getContext(), SetupTimerActivity.class);
+        startActivity(setupTimerIntent);
     }
 
-    // updates the time we display in the TextView
-    private void updateDisplay() {
-        mTimeDisplay.setText(
-                new StringBuilder()
-                        .append(pad(mHour)).append(":")
-                        .append(pad(mMinute)));
-    }
 
     private static String pad(int c) {
         if (c >= 10)
@@ -59,22 +37,4 @@ public class TimersListActivity extends Activity
             return "0" + String.valueOf(c);
     }
 
-    private TimePickerDialog.OnTimeSetListener mTimeSetListener =
-            new TimePickerDialog.OnTimeSetListener() {
-                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                    mHour = hourOfDay;
-                    mMinute = minute;
-                    updateDisplay();
-                }
-            };
-
-    @Override
-    protected Dialog onCreateDialog(int id) {
-        switch (id) {
-            case TIME_DIALOG_ID:
-                return new TimePickerDialog(this,
-                        mTimeSetListener, mHour, mMinute, true);
-        }
-        return null;
-    }
 }
